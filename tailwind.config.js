bf0849895a7ba63f715268d1caf4d4a51ac7faed
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-05-04 19:56:18 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      colors: {
+        primary: '#0284c7',
+        secondary: '#64748b',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }    
+    },
+  },
+  plugins: [],
+}
+
