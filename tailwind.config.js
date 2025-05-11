695492f1148d13830c192f48f688e11be87bd2d6
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-05-11 15:04:28 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,10 @@ module.exports = {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
         }
-      }    
+      },
+       fontFamily: {
+        'fira': ['Fira Sans', 'sans-serif'],
+      },
     },
   },
   plugins: [],
