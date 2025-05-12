c271e5d5a8588b238d59706cab38050445ca8b5f
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-05-12 11:08:34 +0700

    update

--- js/index.js
+++ js/index.js
@@ -19,3 +19,35 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 })
+
+ setInterval(waktu, 1000)
+    function waktu(){
+      let time = new Date()
+      let hour = time.getHours()
+      let min = time.getMinutes()
+      am_pm = "AM"
+
+      if(hour >= 12){
+        if(hour > 12) hour -+12
+        am_pm = "AM"
+      }
+
+      hour = hour < 10 ? "0" + hour : hour
+      min = min < 10 ? "0" + + min : min
+      let currenTime = hour + ":" + min + " " + am_pm
+
+      document.getElementById("jam").innerHTML = currenTime
+    }
+
+    function tanggal(){
+      const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+      const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+      let tgl = new Date()
+      let tanggal = tgl.getDate()
+      let hari = tgl.getDay()
+      let bulan = tgl.getMonth()
+      let currentDate = weekday[hari] + " " + tanggal + " " + month[bulan]
+
+      document.getElementById("tanggal").innerHTML = currentDate
+    }
+
